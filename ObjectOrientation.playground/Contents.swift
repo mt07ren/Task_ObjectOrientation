f1df88a3642561b4d25bb66393223b925803d718
sakiyamaK <sakiyama.k@gmail.com>
2020-03-01 14:25:05 +0900

    update

--- ObjectOrientation.playground/Contents.swift
+++ ObjectOrientation.playground/Contents.swift
@@ -2,41 +2,42 @@ import Foundation
 
 /*
  課題
-勇者、戦士クラスで似たような処理を人クラスを作り共通化させてください
-またnameパラメータをカプセル化して下の(*1)がエラーになるようにしてください
+ 勇者、戦士クラスで似たような処理を人クラスを作り共通化させてください
+ またnameパラメータをfileprivateを使ってカプセル化して下の(*1)がエラーになるようにしてください
  変わりに(*2)で初期化パラメータを入れてnameを決定してください
  */
 
-class Yusha {
-    var hp: Int = 10
-    var mp: Int = 10
-    var name: String = ""
+
+class Human {
+    fileprivate var hp: Int = 10
+    fileprivate var mp: Int = 10
+    fileprivate var name: String = ""
+
+    init(name: String) {
+        self.name = name
+    }
 
     func attackKen() {
         print("\(name)は剣で攻撃")
     }
+}
 
+class Yusha: Human {
     func attackMaho() {
         print("\(name)は魔法で攻撃")
     }
 }
 
-class Senshi {
-    var hp = 10
-    var name: String = ""
-
-    func attackKen() {
-        print("戦士は剣で攻撃")
-    }
+class Senshi: Human {
 }
 
 //(*2)
-let senshi = Senshi()
-let yusha = Yusha()
+let senshi = Senshi(name: "浜田")
+let yusha = Yusha(name: "松本")
 
 //(*1)
-senshi.name = "浜田"
-yusha.name = "松本"
+//senshi.name = "浜田"
+//yusha.name = "松本"
 
 
 senshi.attackKen()
